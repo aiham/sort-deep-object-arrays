d0f47bea81f28cbf18e324e9605bbaeebc8b4f09
Aiham Hammami <aiham@users.noreply.github.com>
2019-07-29 15:20:16 +1000

    Add error handling to CLI script

--- bin/sort-deep-object-arrays.js
+++ bin/sort-deep-object-arrays.js
@@ -1,16 +1,32 @@
 #!/usr/bin/env node
 
-var sortDeepObjectArrays = require('../index');
+var sortDeepObjectArrays = require('../');
+var fs = require('fs');
+var path = require('path');
 
-(function(){
+(function() {
   if (process.argv.length <= 2) {
-    console.log('Usage: sort-deep-object-arrays.js input-file-path');
+    console.log('Usage: sort-deep-object-arrays /path/to/input/file.json');
     return;
   }
 
-  require('fs').readFile(process.argv[2], 'utf8', function(err, data){
-    var json = JSON.parse(data);
-    var sorted = sortDeepObjectArrays(json);
-    console.log(JSON.stringify(sorted, null, 2));
+  var filePath = path.resolve(process.argv[2]);
+  fs.readFile(filePath, 'utf8', function(err, text) {
+    if (err) {
+      console.error('Failed to read file (' + filePath + '): ', err);
+      process.exit(1);
+    }
+    var data;
+    try {
+      data = JSON.parse(text);
+    } catch (e) {
+      console.error('Failed to parse JSON file (' + filePath + '): ', e);
+      console.error('--------FILE CONTENT START----------');
+      console.error(text);
+      console.error('--------FILE CONTENT END------------');
+      process.exit(1);
+    }
+    var sortedData = sortDeepObjectArrays(data);
+    console.log(JSON.stringify(sortedData, null, 2));
   });
 })();
